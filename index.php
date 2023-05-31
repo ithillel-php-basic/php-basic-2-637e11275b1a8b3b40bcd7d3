aee52b99fc91697e516af56b5f6f84c334f01b28
Olha Kharabet <kharabet.olga@gmail.com>
2023-05-31 20:35:27 +0200

    Added reading data from database

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+
 require 'helpers.php';
+require 'config.php';
+
+/** @var $database */
+$serverName = $database['host'];
+$dbName = $database['db_name'];
+$userName = $database['user_name'];
+$password =  $database['password'];
 
 $status = [
     'backlog',
@@ -8,52 +16,39 @@ $status = [
     'done'
 ];
 
-$project = [
-    "Вхідні",
-    "Навчання",
-    "Робота",
-    "Домашні справи",
-    "Авто"
-];
+try {
+	$connection = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$tasksList = [
-    [
-        'name' => 'Співбесіда в ІТ компанії',
-        'date' => '01.07.2023',
-        'category' => $project[2],
-        'status' => $status[0]
-    ],
-    [
-        'name' => 'Виконати тестове завдання',
-        'date' => '25.07.2023',
-        'category' => $project[2],
-        'status' => $status[0]
-    ],
-    [
-        'name' => 'Зробити завдання до першого уроку',
-        'date' => '27.04.2023',
-        'category' => $project[1],
-        'status' => $status[3]
-    ],
-    [
-        'name' => 'Зустрітись з друзями',
-        'date' => '14.05.2023',
-        'category' => $project[0],
-        'status' => $status[1]
-    ],
-    [
-        'name' => 'Купити корм для кота',
-        'date' => null,
-        'category' => $project[3],
-        'status' => $status[2]
-    ],
-    [
-        'name' => 'Замовити піцу',
-        'date' => null,
-        'category' => $project[3],
-        'status' => $status[1]
-    ],
-];
+	$projectCollection = $connection->prepare("SELECT * FROM projects WHERE user_id = (SELECT id FROM users WHERE email='sblessed6@surveymonkey.com')");
+	$projectCollection->execute();
+	$result = $projectCollection->setFetchMode(PDO::FETCH_ASSOC);
+	$projectsList = $projectCollection->fetchAll();
+
+	foreach ($projectsList as $item) {
+		$project[] = $item['name'];
+	}
+
+	$taskCollection = $connection->prepare("SELECT * FROM tasks WHERE user_id = (SELECT id FROM users WHERE email='sblessed6@surveymonkey.com')");
+	$taskCollection->execute();
+	$result = $taskCollection->setFetchMode(PDO::FETCH_ASSOC);
+	$tasks = $taskCollection->fetchAll();
+
+	foreach ($tasks as $task) {
+
+		$category = getCategoryName($projectsList, $task['project_id']);
+
+		$tasksList[] = [
+			'name' => $task['title'],
+			'date' => $task['created_at'],
+			'category' => $category,
+			'status' => $task['status']
+		];
+	}
+
+} catch(PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
 
 $kanban = renderTemplate('kanban.php', [
     'tasksList' => $tasksList,
